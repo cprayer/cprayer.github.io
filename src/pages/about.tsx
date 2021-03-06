6b394d5419830f3fd02f30175fd0114ad2f23473
cprayer <cprayer13@gmail.com>
2021-03-06 18:35:24 +0900

    about 임시 변경

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,29 +1,26 @@
 import * as React from "react";
 import { Header, Container, Segment, Icon } from "semantic-ui-react";
-import {withLayout} from "../components/Layout";
+import { withLayout } from "../components/Layout";
 
 const AboutPage = () => {
   return (
     <Container>
       <Segment vertical>
         <Header as="h2">
-          <Icon name="info circle" />
-          <Header.Content>
-            About
-          </Header.Content>
+          <Header.Content>About Me</Header.Content>
         </Header>
       </Segment>
       <Segment vertical>
-        <p>
-          This starter was created by @fabien0102.
-        </p>
-        <p>
-          For any question, I'm on <a href="https://discord.gg/2bz8EzW" target="blank">discord #reactiflux/gatsby</a>
-        </p>
-        <p>
-          For any issues, any PR are welcoming
-          <a href="https://github.com/fabien0102/gatsby-starter/issues" target="blank"> on this repository</a>
-        </p>
+        <a href="https://github.com/cprayer">https://github.com/cprayer</a>
+        <br />
+        <a href="https://codeforces.com/profile/cprayer">
+          https://codeforces.com/profile/cprayer
+        </a>
+        <br />
+        <a href="https://linkedin.com/in/taemin-shin">
+          https://linkedin.com/in/taemin-shin
+        </a>
+        <br />
       </Segment>
     </Container>
   );
